af18c41f95a9cc850a89eb95b486b49c4a9bdcbb
bernd.nordhausen-at-649524209221 <bernd.nordhausen@accenture.com>
2020-04-26 16:48:12 +0800

    .

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "google project ID"
+  type        = string
+}
+
+variable "zone" {
+  description = "zone"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+}
+
+variable "network_name" {
+  description = "Name of Network"
+  type = string
+}
