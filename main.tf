893490fe4ee30f373880751133be0866a0016253
bernd.nordhausen-at-649524209221 <bernd.nordhausen@accenture.com>
2020-04-26 16:30:25 +0800

    .

---
+++ main.tf
@@ -0,0 +1,16 @@
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 8
+}
+
+// A single Google Cloud Engine instance
+resource "google_compute_instance" "default" {
+ name         = "flask-vm-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = "us-west1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
